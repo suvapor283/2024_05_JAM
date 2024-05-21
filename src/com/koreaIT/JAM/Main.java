10ccf4ee7137eddc840dd2c796403eca5b6a2f86
suvapor283 <suvapor8632@gmail.com>
2024-05-21 15:51:20 +0900

    article write 진행 중

--- src/com/koreaIT/JAM/Main.java
+++ src/com/koreaIT/JAM/Main.java
@@ -1,5 +1,9 @@
 package com.koreaIT.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,6 +11,10 @@ import java.util.Scanner;
 import com.koreaIT.JAM.dto.Article;
 
 public class Main {
+	private static final String URL = "jdbc:mysql://localhost:3306/jdbc_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -29,9 +37,43 @@ public class Main {
 				System.out.println("== 게시물 작성 ==");
 
 				System.out.printf("제목 : ");
-				String title = sc.nextLine();
+				String title = sc.nextLine().trim();;
 				System.out.printf("내용 : ");
-				String body = sc.nextLine();
+				String body = sc.nextLine().trim();
+				
+				Connection connection = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = '"+ title +"'";
+					sql += ", `body` = '"+ body +"';";
+
+					pstmt = connection.prepareStatement(sql);
+					pstmt.executeUpdate();
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					if (pstmt != null) {
+						try {
+							pstmt.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+					if (connection != null) {
+						try {
+							connection.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+				}
 
 				articles.add(new Article(lastArticleNum, title, body));
 
