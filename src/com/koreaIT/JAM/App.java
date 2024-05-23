394862b3c9b832363927f1fa308b1b32056cab8f
suvapor283 <suvapor8632@gmail.com>
2024-05-23 15:53:30 +0900

    modify, delete, detail에 DBUtil 적용

--- src/com/koreaIT/JAM/App.java
+++ src/com/koreaIT/JAM/App.java
@@ -56,89 +56,89 @@ public class App {
 				}
 
 				else if (cmd.startsWith("article modify ")) {
-					//
 
-					try {
-						int id = Integer.parseInt(cmd.split(" ")[2]);
+					int id = Integer.parseInt(cmd.split(" ")[2]);
 
-						System.out.println("== 게시물 수정 ==");
+					SecSql sql = new SecSql();
+					sql.append("SELECT count(*)");
+					sql.append("FROM article");
+					sql.append("WHERE id = ?;", id);
 
-						System.out.printf("수정할 제목 : ");
-						String title = sc.nextLine().trim();
-						System.out.printf("수정할 내용 : ");
-						String body = sc.nextLine().trim();
+					int articleCount = DBUtil.selectRowIntValue(connection, sql);
 
-						SecSql sql = new SecSql();
-						sql.append("UPDATE article SET");
-						sql.append("updateDate = NOW()");
-						sql.append(", title = ?", title);
-						sql.append(", `body` = ?", body);
-						sql.append("WHERE id = ?;", id);
-
-						DBUtil.update(connection, sql);
-						System.out.printf("%d번 게시물이 수정되었습니다.%n", id);
-
-					} catch (NumberFormatException e) {
-						System.out.println("명령어를 다시 입력해주세요.");
+					if (articleCount == 0) {
+						System.out.printf("%d번 게시물이 존재하지 않습니다.%n", id);
 						continue;
 					}
+
+					System.out.println("== 게시물 수정 ==");
+
+					System.out.printf("수정할 제목 : ");
+					String title = sc.nextLine().trim();
+					System.out.printf("수정할 내용 : ");
+					String body = sc.nextLine().trim();
+
+					sql = new SecSql();
+					sql.append("UPDATE article SET");
+					sql.append("updateDate = NOW()");
+					sql.append(", title = ?", title);
+					sql.append(", `body` = ?", body);
+					sql.append("WHERE id = ?;", id);
+
+					DBUtil.update(connection, sql);
+
+					System.out.printf("%d번 게시물이 수정되었습니다.%n", id);
 				}
 
 				else if (cmd.startsWith("article delete ")) {
-					//
-					try {
-						int id = Integer.parseInt(cmd.split(" ")[2]);
 
-						System.out.println("== 게시물 삭제 ==");
+					int id = Integer.parseInt(cmd.split(" ")[2]);
 
-						SecSql sql = new SecSql();
-						sql.append("DELETE FROM article");
-						sql.append("WHERE id = ?;", id);
+					SecSql sql = new SecSql();
+					sql.append("SELECT count(*)");
+					sql.append("FROM article");
+					sql.append("WHERE id = ?;", id);
 
-						DBUtil.delete(connection, sql);
-						System.out.printf("%d번 게시물이 삭제되었습니다.%n", id);
+					int articleCount = DBUtil.selectRowIntValue(connection, sql);
 
-					} catch (NumberFormatException e) {
-						System.out.println("명령어를 다시 입력해주세요.");
+					if (articleCount == 0) {
+						System.out.printf("%d번 게시물이 존재하지 않습니다.%n", id);
 						continue;
 					}
+
+					System.out.println("== 게시물 삭제 ==");
+
+					sql = new SecSql();
+					sql.append("DELETE FROM article");
+					sql.append("WHERE id = ?;", id);
+
+					DBUtil.delete(connection, sql);
+					System.out.printf("%d번 게시물이 삭제되었습니다.%n", id);
 				}
 
 				else if (cmd.startsWith("article detail ")) {
-					//
-					try {
-						int id = Integer.parseInt(cmd.split(" ")[2]);
 
-						System.out.println("== 게시물 조회 ==");
+					int id = Integer.parseInt(cmd.split(" ")[2]);
 
-						List<Article> articles = new ArrayList<>();
+					SecSql sql = new SecSql();
+					sql.append("SELECT *");
+					sql.append("FROM article");
+					sql.append("WHERE id = ?;", id);
 
-						SecSql sql = new SecSql();
-						sql.append("SELECT * FROM article");
-						sql.append("ORDER BY id DESC;");
+					Map<String, Object> articleMap = DBUtil.selectRow(connection, sql);
 
-						List<Map<String, Object>> articleListMap = DBUtil.selectRows(connection, sql);
-
-						for (Map<String, Object> articleMap : articleListMap) {
-							articles.add(new Article(articleMap));
-						}
-
-						if (articles.isEmpty()) {
-							System.out.println("게시물이 존재하지 않습니다");
-							continue;
-						}
-
-						for (Article article : articles) {
-							if (article.id == id) {
-								System.out.println("	번호	|	     작성일		|     제목	|	내용");
-								System.out.printf("	%d	|	%s	|     %s 	|	%s%n", article.id, article.regDate,
-										article.title, article.body);
-							}
-						}
-					} catch (NumberFormatException e) {
-						System.out.println("명령어를 다시 입력해주세요.");
+					if (articleMap.isEmpty()) {
+						System.out.printf("%d번 게시물은 존재하지 않습니다.%n", id);
 						continue;
 					}
+					Article article = new Article(articleMap);
+
+					System.out.printf("== %d번 게시물 조회 ==%n", id);
+					System.out.printf("번호 : %d%n", article.id);
+					System.out.printf("작성일 : %s%n", article.regDate);
+					System.out.printf("수정일 : %s%n", article.updateDate);
+					System.out.printf("제목 : %s%n", article.title);
+					System.out.printf("내용 : %s%n", article.body);
 				}
 
 				else if (cmd.equals("article list")) {
